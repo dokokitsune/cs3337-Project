63e9183a569cc174af1cc19ed9afac5a3894e337
dokokitsune <stakin101@gmail.com>
2024-04-23 23:45:05 -0700

    alpha front page reformat

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
-    path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
-    path('postbook', views.postbook, name='postbook'),
-    path('displaybooks', views.displaybooks, name='displaybooks'),
-    path('mybooks', views.mybooks, name='mybooks'),
+    path("", views.index, name="index"),
+    path("aboutus/", views.about_us, name="about_us"),
+    path("book_detail/<int:book_id>", views.book_detail, name="book_detail"),
+    path("book_delete/<int:book_id>", views.book_delete, name="book_delete"),
+    path("postbook", views.postbook, name="postbook"),
+    path("displaybooks", views.displaybooks, name="displaybooks"),
+    path("mybooks", views.mybooks, name="mybooks"),
 ]
-
